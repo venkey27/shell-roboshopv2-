d441ba7eb8288da92a8ec8a1d92e4c66874927a9
venkey27 <venkey271200@gmail.com>
2026-06-02 14:53:09 -0400

    special variables

--- common.sh
+++ common.sh
@@ -3,7 +3,7 @@ sudo mkdir -p $LOGS_FOLDER
 sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
 sudo chmod -R 755 $LOGS_FOLDER
 LOGS_FILE="$LOGS_FOLDER/$0.log"
-
+SCRIPT_DIR=$PWD
 
 USERID=$(id -u)
 R='\e[31m'
